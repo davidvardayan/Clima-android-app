a57b08295068c2afd3dade77d401fdebfcd64881
David <dav-ss@mail.ru>
2019-05-28 06:12:11 -0700

    Creating change city Intent

--- app/src/main/java/com/davidvardanyan/climapm/WeatherController.java
+++ app/src/main/java/com/davidvardanyan/climapm/WeatherController.java
@@ -2,6 +2,7 @@ package com.davidvardanyan.climapm;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
@@ -11,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -64,6 +66,14 @@ public class WeatherController extends AppCompatActivity {
         ImageButton changeCityButton = findViewById(R.id.changeCityButton);
 
 
+        changeCityButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent myIntent = new Intent(WeatherController.this,ChangeCityController.class);
+            }
+        });
+
+
     }
 
 
@@ -71,6 +81,8 @@ public class WeatherController extends AppCompatActivity {
     public void onResume() {
         super.onResume();
         Log.d("Clima", "onResume() called");
+        Intent myIntent = getIntent();
+        String city =myIntent.getStringExtra("City");
         Log.d("Clima", "Getting weather for current location");
         getWeatherForCurrentLocation();
     }
